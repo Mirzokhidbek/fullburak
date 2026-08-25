4ad666f687b3830f4bd0b6c5871a23a8e27c8e28
Mirzokhidbek <mirodev@sju.ac.kr>
2026-08-25 17:49:34 +0900

    remove restaurant controller and associated admin router configuration

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import path from "path";
 import router from "./router";
-import routerAdmin from "./router-admin";
 
 /** 1-ENTRANCE **/
 const app = express();
@@ -16,7 +15,6 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /** 4-ROUTERS **/
-app.use("/admin", routerAdmin); // SSR: EJS
-app.use("/", router);           // SPA: REACT
+app.use("/", router);
 
 export default app;
