2a5ce280217a117784f3f0380295773349698317
Mirzokhidbek <mirodev@sju.ac.kr>
2026-08-26 01:14:05 +0900

    docs: add project naming and logging standards to train.ts

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,8 @@
-console.log("TRAIN AREA!");
+/* Project Standards:
+  - Logging standards
+  - Naming standards
+      function, method, variable => CAMEL
+      class => PASCAL
+      folder, file => KEBAB-CASE / CAMEL
+      css => KEBAB-CASE
+*/
