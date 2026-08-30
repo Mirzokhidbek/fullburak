4b6df36b10f83b3015bd49ca3aee5d65e998fb46
Mirzokhidbek <mirodev@sju.ac.kr>
2026-08-31 04:26:51 +0900

    refactor: update MongoDB connection string handling and configure server listen address and ports

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,17 @@ import app from "./app";
 
 mongoose.set("strictQuery", true);
 
+const mongoUrl = process.env.MONGO_URL as string;
+
 mongoose
-  .connect(process.env.MONGO_URL as string, {})
-  .then((data) => {
+  .connect(mongoUrl, {})
+  .then(() => {
     console.log("MongoDB connection succeed");
-    const PORT = process.env.PORT ?? 3003;
-    app.listen(PORT, function () {
+    const PORT = Number(process.env.PORT) || 3001;
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`The server is running successfully on port: ${PORT}`);
+      console.log(`BSSR Admin: http://localhost:${PORT}/admin`);
+      console.log(`SPA REST API: http://localhost:${PORT}/`);
     });
   })
   .catch((err) => console.log("ERROR on connection MongoDB", err));
